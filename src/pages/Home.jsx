52dbc36349ff95db630ded2a31ef74adce9854d6
Roo <rawan.ahmed.2006.10@gmail.com>
2026-05-02 20:03:56 +0300

    adding the initial popularProducts section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import BestSelling from '../components/homePageComponents/BestSelling'
-import PromotionCardsSection from '../components/homePageComponents/PromotionCardsSection'
-import PromotionHeroSection from '../components/homePageComponents/PromotionHeroSection'
+import React from "react";
+import BestSelling from "../components/homePageComponents/BestSelling";
+import PromotionCardsSection from "../components/homePageComponents/PromotionCardsSection";
+import PromotionHeroSection from "../components/homePageComponents/PromotionHeroSection";
+import MostPopular from "../components/homePageComponents/MostPopular";
 
 export default function Home() {
-  return <>
-    <PromotionHeroSection />
-    <PromotionCardsSection />
-    <BestSelling />
-  </>
+  return (
+    <>
+      <PromotionHeroSection />
+      <PromotionCardsSection />
+      <BestSelling />
+      <MostPopular />
+    </>
+  );
 }
